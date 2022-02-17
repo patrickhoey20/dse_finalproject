9b962595074b09548c83347f1c7a7b367d33635f
Reid Gutz <reidgutz20204@u.northwestern.edu>
2022-02-17 14:56:23 -0600

    stored in list

--- ProjectData.py
+++ ProjectData.py
@@ -5,7 +5,7 @@ from string import ascii_lowercase
 
 
 # Get links for all player pages
-
+linklist = []
 for i in ascii_lowercase:
  	url = "https://www.baseball-reference.com/players/" + i + "/"
  	r = requests.get(url)
@@ -14,7 +14,8 @@ for i in ascii_lowercase:
  	x = x.find('div',{"class": "section_content"})
  	z = x.find_all('a')
  	for j in z:
- 		print(j['href'])
+ 		linklist.append(['href'])
+
 
 # url = "https://chicago.craigslist.org/search/zip"
 # #print(url)
