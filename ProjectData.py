e69b09663e69433ed5d3d18f129e8778f7bbaeef
Patrick Hoey <patrickhoey2024@u.northwestern.edu>
2022-02-21 17:12:25 -0600

    delete original ProjectData.py

--- ProjectData.py
+++ ProjectData.py
@@ -7,14 +7,15 @@ from string import ascii_lowercase
 # Get links for all player pages
 linklist = []
 for i in ascii_lowercase:
- 	url = "https://www.baseball-reference.com/players/" + i + "/"
- 	r = requests.get(url)
- 	soup = BeautifulSoup(r.text, 'html.parser')
- 	x = soup.body
- 	x = x.find('div',{"class": "section_content"})
- 	z = x.find_all('a')
- 	for j in z:
- 		linklist.append(['href'])
+    url = "https://www.baseball-reference.com/players/" + i + "/"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    x = soup.body
+    x = x.find('div',{"class": "section_content"})
+    z = x.find_all('a')
+    for j in z:
+        link = "http://www.baseball-reference.com" + j['href']
+        linklist.append(link)
 
 
 # url = "https://chicago.craigslist.org/search/zip"
