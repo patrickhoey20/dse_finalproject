3df78a8c7fca08aa7f9b99f5f22ac4cc0b1d3074
Patrick Hoey <patrickhoey2024@u.northwestern.edu>
2022-03-03 21:13:42 -0600

    rename pitchers_regression

---
+++ pitchers_prediction.py
@@ -0,0 +1,18 @@
+import pandas
+
+# Coefficients from logistic regression pasted from Jupyter file
+coeffs = {'years_played': 0.0020428039642415513,
+ 'career_era': -1.1158026287597493,
+ 'career_whip': -1.9318618093573272,
+ 'career_ip': -0.0012619974204642326,
+ 'career_wl': -0.8934375712804276,
+ 'career_wins': 0.03778412738264687,
+ 'career_so': -0.0014222222823067736,
+ 'career_war': 0.0830667170029248,
+ 'career_saves': 0.00818862965699141,
+ 'world_series_wins': 0.30117298303976586,
+ 'cy_young_awards': 0.2982898970935313,
+ 'mvps': -0.5136674329481301,
+ 'era_titles': 0.3401397317783753,
+ 'all_star_apps': 0.28873683194134486}
+
